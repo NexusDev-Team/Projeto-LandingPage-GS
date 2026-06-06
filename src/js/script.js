0c970f7e138fbfe265dfb61afc1f23b89d2ae30e
João <contatojbdev@gmail.com>
2026-06-06 12:03:55 -0300

    feat: adicionando botões que mudam a cor do texto

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,24 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }
 })
 
-function trocar(cor){
-    document.body.style.background=cor;
+function trocarTema(tema) {
+    const root = document.documentElement;
 
+    if (tema === 'laranja') {
+        root.style.setProperty('--cor-destaque', '#FF9A3C');
+        root.style.setProperty('--cor-destaque-claro', '#FF9A3C');
+        root.style.setProperty('--descricao-card', '#FF9A3C');
+    }
+
+    if (tema === 'branco') {
+        root.style.setProperty('--cor-destaque', '#F2F2F2');
+        root.style.setProperty('--cor-destaque-claro', '#F2F2F2');
+        root.style.setProperty('--descricao-card', '#F2F2F2');
+    }
+
+    if (tema === 'cinza') {
+        root.style.setProperty('--cor-destaque', '#9A9A9A');
+        root.style.setProperty('--cor-destaque-claro', '#9A9A9A');
+        root.style.setProperty('--descricao-card', '#9A9A9A');
+    }
 }
